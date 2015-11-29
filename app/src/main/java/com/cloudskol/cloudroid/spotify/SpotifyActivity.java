5391ef56659686a8a4b737d5afa1c02b1019ebef
Thamizharasu <zarub2k@gmail.com>
2015-11-29 13:18:10 +0530

    Exception and AsynTask code has been introduced

--- app/src/main/java/com/cloudskol/cloudroid/spotify/SpotifyActivity.java
+++ app/src/main/java/com/cloudskol/cloudroid/spotify/SpotifyActivity.java
@@ -5,10 +5,21 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 import com.cloudskol.cloudroid.R;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * @author tham
+ *
+ * Activity class for spotify application
+ */
 public class SpotifyActivity extends AppCompatActivity {
 
     @Override
@@ -27,6 +38,28 @@ public class SpotifyActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        renderMovies();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_spotify, menu);
+        return true;
+    }
+
+    private void renderMovies() {
+
+        String[] moviesArray = {
+                "Golden eye", "The Shooter", "Tomorrow never dies"
+        };
+
+        final ArrayList<String> movies = new ArrayList<String>(Arrays.asList(moviesArray));
+
+        final GridView spotifyGridView = (GridView) findViewById(R.id.gridView_spotify);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+                R.layout.list_item_spotify, R.id.list_item_spotify_textview, movies);
+        spotifyGridView.setAdapter(arrayAdapter);
+    }
 }
