c303208831f83e5694ac58277cf5284095f64491
Thamizharasu <zarub2k@gmail.com>
2015-12-02 07:09:15 +0530

    movie details path added

--- app/src/main/java/com/cloudskol/cloudroid/common/CloudroidPropertyKeys.java
+++ app/src/main/java/com/cloudskol/cloudroid/common/CloudroidPropertyKeys.java
@@ -8,6 +8,7 @@ package com.cloudskol.cloudroid.common;
 public interface CloudroidPropertyKeys {
     String SPOTIFY_DISCOVER_MOVIE = "cloudroid.spotify.discover.movie";
     String SPOTIFY_API_KEY = "cloudroid.spotify.api.key";
-    String SPOTIFY_MOVIE_POSTER="cloudroid.spotify.movie.poster";
-    String SPOTIFY_MOVIE_POSTER_W185="cloudroid.spotify.movie.poster.w185";
+    String SPOTIFY_MOVIE_POSTER = "cloudroid.spotify.movie.poster";
+    String SPOTIFY_MOVIE_POSTER_W185 = "cloudroid.spotify.movie.poster.w185";
+    String SPOTIFY_MOVIE_DETAILS = "cloudroid.spotify.movie.details";
 }
